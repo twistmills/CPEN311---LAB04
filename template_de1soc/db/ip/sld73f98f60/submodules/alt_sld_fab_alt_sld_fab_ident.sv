d7caac06c1ceac8e6327f1c38a016a6f0b38cce0
Ryan Brown <ryanbrown919@gmail.com>
2024-06-13 22:42:07 -0700

    Code Crack fully functional

--- template_de1soc/db/ip/sld73f98f60/submodules/alt_sld_fab_alt_sld_fab_ident.sv
+++ template_de1soc/db/ip/sld73f98f60/submodules/alt_sld_fab_alt_sld_fab_ident.sv
@@ -20,7 +20,7 @@
 // Altera Identification hub
 //
 // Parameters
-//   DESIGN_HASH   : d714e2e994ae39f51fe3
+//   DESIGN_HASH   : 7342793f6ce3f5dc8ee6
 //   COUNT         : 1
 //   ROM_WIDTHS    : 4
 //   LATENCIES     : 0
@@ -39,7 +39,7 @@ module alt_sld_fab_alt_sld_fab_ident
 );
 
 
-wire [127:0] data_0 = { mixed, 12'h0, 32'h0, 80'hd714e2e994ae39f51fe3 };
+wire [127:0] data_0 = { mixed, 12'h0, 32'h0, 80'h7342793f6ce3f5dc8ee6 };
 
 reg [3:0] result_0;
 always @(address_0 or data_0) begin
